a1ab99b6df748a5de30bdf95eace34e1c659a6e3
James Nguyo <james.nguyo@saner.gy>
2020-03-13 14:39:04 +0300

    first commit

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,36 @@
+declare module "@salesforce/resourceUrl/DKImg" {
+    var DKImg: string;
+    export default DKImg;
+}
+declare module "@salesforce/resourceUrl/GraphicsPack" {
+    var GraphicsPack: string;
+    export default GraphicsPack;
+}
+declare module "@salesforce/resourceUrl/GraphicsPackExample" {
+    var GraphicsPackExample: string;
+    export default GraphicsPackExample;
+}
+declare module "@salesforce/resourceUrl/Sanergy_Logo" {
+    var Sanergy_Logo: string;
+    export default Sanergy_Logo;
+}
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/caseOpportunityStylesheet" {
+    var caseOpportunityStylesheet: string;
+    export default caseOpportunityStylesheet;
+}
+declare module "@salesforce/resourceUrl/freshlife_logo" {
+    var freshlife_logo: string;
+    export default freshlife_logo;
+}
+declare module "@salesforce/resourceUrl/jqueryTableSorter" {
+    var jqueryTableSorter: string;
+    export default jqueryTableSorter;
+}
+declare module "@salesforce/resourceUrl/sanergylogoCropped" {
+    var sanergylogoCropped: string;
+    export default sanergylogoCropped;
+}
